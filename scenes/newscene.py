b44fc8fcef10fe6ce886058d64836eccc6db5f66
jabosso <bossojohan@gmail.com>
2019-10-29 17:07:14 +0100

    nescene update

--- scenes/newscene.py
+++ scenes/newscene.py
@@ -1,51 +1,105 @@
 from manta import *
 dim = 3
 res = 64
-gs =vec3(res, res, res)
+gs =vec3(res*2, res, res)
+#-------------- Create the solver for the scene ----------------------
 s = Solver(name='main', gridSize=gs, dim= dim)
 s.timestep = 1.0
 
+minParticles = pow(2, dim)
+radiusFactor = 0.8
 
 flags = s.create(FlagGrid)
-#vel = s.create(MACGrid)
-#density = s.create(RealGrid)
-#pressure = s.create(RealGrid)
+#------------------------------------------------------------
 
-#initialize my scene geometry
+#------------------------initialize my scene geometry-------------------
 flags.initDomain() #create an empty box
 flags.fillGrid()
 phi = s.create(LevelsetGrid)
 
+vel = s.create(MACGrid)
+velOld = s.create(MACGrid)
+pressure = s.create(RealGrid)
+tmpVec3 = s.create(VecGrid)
+tstGrid = s.create(RealGrid)
+
+
 pp = s.create(BasicParticleSystem)
+pVel = pp.create(PdataVec3)
+pTest = pp.create(PdataReal)
 mesh = s.create(Mesh)
 
-bWidth = 1
-flags.initDomain(boundaryWidth=bWidth)
+pindex = s.create(ParticleIndexSystem)
+gpi = s.create(IntGrid)
 
+bWidth = 1
+#fluidVel = 0
+flags.initDomain(boundaryWidth=bWidth)
+#--------------------------------------------------------------------------
+fluidDrop = Box(parent=s, p0=gs * vec3(0.0, 0.6, 0.05), p1=gs * vec3(0.4,0.7, 0.95))
+fluidVel = Box(parent=s, p0=gs * vec3(0.50, 0.70, 0.18), p1=gs * vec3(0.80, 0.9, 0.4))
+fluidSetVel = vec3(0, -1, 0)
+
+
+#--------------------------------------------------------------------------
 fluidBasin = Box(parent=s, p0=gs * vec3(0, 0, 0), p1=gs * vec3(1.0, 0.1, 1.0))
+
 phi = fluidBasin.computeLevelset()
+#-----------------
+phi.join(fluidDrop.computeLevelset())
+#--------------------------------------------------------------------------
+obstacle1 = Box(parent=s, p0=gs * vec3(0, 0, 0), p1=gs * vec3(0.4, 0.6, 1.0))
+obstacle1.applyToGrid(grid=flags, value=FlagObstacle)
+
+sdfgrad = obstacleGradient(flags)
+sdf = obstacleLevelset(flags)
+bgr = s.create(Mesh)
+sdf.createMesh(bgr)
+#------------------
 
 flags.updateFromLevelset(phi)
 
 sampleLevelsetWithParticles(phi=phi, flags=flags, parts=pp, discretization=2, randomness=0.05)
+
+fluidVel.applyToGrid(grid=vel, value=fluidSetVel)
+mapGridToPartsVec3(source=vel, parts=pp, target=pVel)
+testInitGridWithPos(tstGrid)
+pTest.setConst(0.1)
+
+
 #this is to show the window with simulation
 if 1 and (GUI):
     gui = Gui()
-    gui.show()
-    
+gui.setBackgroundMesh(bgr)
+gui.show()
+gui.pause()    
 #main loop
-for t in range(250):
+for t in range(2050):
     mantaMsg('\nFrame %i, simulation time %f' % (s.frame, s.timeTotal))
     
+    pp.advectInGrid(flags=flags, vel=vel, integrationMode=IntRK4, deleteInObstacle=False)    
+    mapPartsToMAC(vel=vel, flags=flags, velOld=velOld, parts=pp, partVel=pVel, weight=tmpVec3)
+    extrapolateMACFromWeight(vel=vel, distance=2, weight=tmpVec3)  # note, tmpVec3 could be free'd now...
     markFluidCells(parts=pp, flags=flags)
-    #pp.advectInGrid(flags=flags, vel=vel, integrationMode=IntRK4, deleteInObstacle=False)    
-    #advectSemiLagrange(flags=flags, vel=vel, grid=density, order=2)
-    #advectSemiLagrange(flags=flags, vel=vel, grid=vel, order=2)
-    #setWallBcs(flags=flags, vel=vel)
-    #addBuoyancy(density=density, vel=vel, gravity=vec3(0,-6e-4,0), flags=flags)
-    #solvePressure(flags=flags, vel=vel, pressure=pressure)
-    #setWallBcs(flags=flags, vel=vel)
-    
+
+    extrapolateLsSimple(phi=phi, distance=4, inside=True);
+
+    gridParticleIndex(parts=pp, flags=flags, indexSys=pindex, index=gpi)
+    unionParticleLevelset(pp, pindex, flags, gpi, phi, radiusFactor)
+    resetOutflow(flags=flags, parts=pp, index=gpi, indexSys=pindex)
+
+    addGravity(flags=flags, vel=vel, gravity=(0, -0.001, 0))
+    setWallBcs(flags=flags, vel=vel)
+    solvePressure(flags=flags, vel=vel, pressure=pressure, phi=phi)
+    setWallBcs(flags=flags, vel=vel)
+    pVel.setSource(vel, isMAC=True)
+    pTest.setSource(tstGrid);
+    adjustNumber(parts=pp, vel=vel, flags=flags, minParticles=1 * minParticles, maxParticles=2 * minParticles,
+                phi=phi,
+                radiusFactor=radiusFactor)
+    extrapolateMACSimple(flags=flags, vel=vel)
+
+    flipVelocityUpdate(vel=vel, velOld=velOld, flags=flags, parts=pp, partVel=pVel, flipRatio=0.97)
     if (dim == 3):
         phi.createMesh(mesh)
 
